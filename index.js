cee090b5451887f9d5f522f53d3c8ea913594c45
html <newparas11422@gmail.com>
2022-05-05 15:18:18 +0530

    variables , data types and operators

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log("hello world")
+
+var a = 78;
+var b = 'paras'
+ c =  23.44
+ console.log(+c);
+
+ //Operators in js
+ //  In 3 + 4 '+' is the operators and 3,4 are oprands 
+//  1. unary operators - It has single operand (c = -c)
+// examaples of unary operators
+c = -c
+console.log( "example of unary operators " + c);
+
+// 2. Binary operator - it has two operand (c = c+5 )
+// example of binary operator
+c = c+5
+console.log("example of  binary operators " + c);
+
+// some arithmetic operators 
+let n1 = 2
+let n2 = 5
+
+// console.log("the value of n1 + n2 " + n1 + n2);
+console.log("the value of n1 + n2 is " +( n1 + n2));
+console.log("the value of n1 - n2 is " +( n1 - n2));
+console.log("the value of n1 * n2 is " +( n1 * n2));
+console.log("the value of n1 / n2 is " +( n1 / n2));
+
+console.log("the value of n1 ** n2 is " +( n1 ** n2));
+
+// understand n++
+console.log("the value of n1 ++ is " +( n1 ++ ));
+console.log("the value of n1 ++ is " +( n1 ++ ));
+p1=1
+p1++
+p1++
+p1++
+console.log(p1);
+
+o1=1
+o1--
+console.log(o1--);
+
+// console.log("the value of n1 ** n2 is " +( n1 ** n2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = 10
+number ++
+console.log(number);
