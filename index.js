700f8610a953a51e6b698b4c9ee91cd899213b25
html <newparas11422@gmail.com>
2022-05-06 02:25:14 +0530

    function

--- index.js
+++ index.js
@@ -1,78 +1,52 @@
-// switch (key) {
-//     case value:
+let name = 'paras'
+let name2 = 'parker'
+let name3 = 'mohan'
+let name4 = 'rohan'
+let name5 = 'anuj' 
+let endMsg = ` " Good Bye form variable" `
 
-//         break;
 
-//     default:
-//         break;
-// }
-let age = 100;
-switch (age) {
-    case 18: console.log('age is 18');
-        break;
 
-        case  24:
-            console.log('age is 24');
-        break;
-        
-        case 100 :
-            console.log('age is 100');
-            break;
-
-    default: console.log('age is not 18')
-        break;
+//  function f(param , end ) {
+ function f(param , end=' TAKE CARE from javascript ') {
+    
+      console.log( param + ' Good morning and how are you Good morning and how are you Good morning and how are you Good morning and how are you Good morning and how are you' + end )
+     
 }
 
+f(name )
+f(name2, endMsg)
+f(name3)
+f(name4, endMsg)
+f(name5,endMsg)
 
-let myvar = 32
-let myvar1 = 'string'
-let myvar2 = null
-let myvar3 = true
-let myvar4 = false
-let myvar5 = undefined
+// return undefined
+let returnVal = f(name5,endMsg)
+console.log(returnVal);
 
-let intro = {
-    name: 'paras',
-    age: 999,
-    "live in": 'new delhi',
-    function: () => "function in object",
-    greeting: 'good night'
+function add (x,y,z){
+ a = x+y+z
+ return  a
 }
 
-console.log(intro['live in']);
-console.log(intro.name);
-console.log(intro.function());
-
-let array = ['paras', 99, { function: function run() { return ('function in array ') } }, 34, 33,9, 54]
-console.log(array[2].function());
-console.log(array['0']);
-console.log(array.sort());
-console.log(array.push('add a new arrary in the end '));
-console.log(array);
-console.log(array.pop());
-console.log(array);
-console.log(array.shift());
-console.log(array);
-
-console.log(array.unshift('add a new array in the start'));
-console.log(array);
+// this  return something
+let returnVal2 = add(10,3,3)
+console.log(returnVal2);
 
 
+// console.log(name + '  Good morning and how are you');
+// console.log(name2 + '  Good morning and how are you');
+// console.log(name3 + '  Good morning and how are you');
+// console.log(name4 + '  Good morning and how are you');
+// console.log(name5 + '  Good morning and how are you');
 
-let arrray = new Array(10)
-console.log(arrray[0]);
+// console.log(name + f());
+// console.log(name3 + f());
+// console.log(name3 + f());
+// console.log(name4 + f());
+// console.log(name5 + f());
 
-arrray[0] = []
-// arrray[0] = [33,true]
-arrray[0].push('pushed')
-
-console.log(arrray);
-arrray.push('pushed')
-console.log(arrray);
-
-
-
-// document.write(array[5].function())
-
-// document.write(intro.function())
-
+const paras = (param,param2, param3 , param4, param5)=>{
+console.log(param, param2, param3 , param4, param5);
+}
+paras(name, name2, name3 , name4 , name5)
