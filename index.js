2c917d5f5cab44945922fee30de28b7dbef9c626
html <newparas11422@gmail.com>
2022-05-06 02:46:49 +0530

    alert , prompt, confirm

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-let name = 'paras'
-let name2 = 'parker'
-let name3 = 'mohan'
-let name4 = 'rohan'
-let name5 = 'anuj' 
-let endMsg = ` " Good Bye form variable" `
+// alert('this is js tutorial')
 
+// alert return nothing
+// let a = alert('this is js tutorial')
+// console.log(a);
 
+// let b = prompt('Enter your name', '')
+// console.log(b);
 
-//  function f(param , end ) {
- function f(param , end=' TAKE CARE from javascript ') {
-    
-      console.log( param + ' Good morning and how are you Good morning and how are you Good morning and how are you Good morning and how are you Good morning and how are you' + end )
-     
+let c = confirm("are you 18 +")
+console.log(c)
+
+switch (c) {
+    case c:
+        document.write('your age is above 18')
+        break;
+
+    default: document.write('YOUR AGE IS BELOW 18')
+        break;
 }
-
-f(name )
-f(name2, endMsg)
-f(name3)
-f(name4, endMsg)
-f(name5,endMsg)
-
-// return undefined
-let returnVal = f(name5,endMsg)
-console.log(returnVal);
-
-function add (x,y,z){
- a = x+y+z
- return  a
-}
-
-// this  return something
-let returnVal2 = add(10,3,3)
-console.log(returnVal2);
-
-
-// console.log(name + '  Good morning and how are you');
-// console.log(name2 + '  Good morning and how are you');
-// console.log(name3 + '  Good morning and how are you');
-// console.log(name4 + '  Good morning and how are you');
-// console.log(name5 + '  Good morning and how are you');
-
-// console.log(name + f());
-// console.log(name3 + f());
-// console.log(name3 + f());
-// console.log(name4 + f());
-// console.log(name5 + f());
-
-const paras = (param,param2, param3 , param4, param5)=>{
-console.log(param, param2, param3 , param4, param5);
-}
-paras(name, name2, name3 , name4 , name5)
