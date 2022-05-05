a8089125c1c3f319ffb5f0f9c2143c1090100cae
html <newparas11422@gmail.com>
2022-05-06 01:07:04 +0530

    switch, array, object

--- index.js
+++ index.js
@@ -1,51 +1,78 @@
-var str = 'paras is a string my name is paras'
-console.log(str);
+// switch (key) {
+//     case value:
 
-let position = str.indexOf('is') /* first is */
-position = str.lastIndexOf('is') /* last is */
-// position = str.slice(1,2)
-position = str.substr(1,2)
-position = str.replace('paras','replaced')  /* replace first paras */
-position = str.replace(/paras/g,'replaced') /* replace all paras */
-console.log(position)
+//         break;
 
-let paras = ' GOOD MORNING '
-let paras2 = ' GOOD NIGHT '
-console.log(paras.concat(paras2));
-let text = document.getElementById('demo').innerText
+//     default:
+//         break;
+// }
+let age = 100;
+switch (age) {
+    case 18: console.log('age is 18');
+        break;
 
+        case  24:
+            console.log('age is 24');
+        break;
+        
+        case 100 :
+            console.log('age is 100');
+            break;
 
-
-let string01 = '    this is another    string        '
-let removeExtraspaces = string01.trim()
-console.log(string01)
-
-console.log(removeExtraspaces)
-
-let char = paras.charAt(8)
-console.log(char);
-
-
-
-
-
-
-
-
-
-const replaced =()=>{
-    console.log('hi')
-
-    let result = text.replace(/is/g, 'PARAS' )
-    result = text.slice(0,100)
-    result = text.substr(1,100)
-    result = text.replace(/is/g, 'PARAS' )
-    document.getElementById('demo').innerText = result
+    default: console.log('age is not 18')
+        break;
 }
 
-  
+
+let myvar = 32
+let myvar1 = 'string'
+let myvar2 = null
+let myvar3 = true
+let myvar4 = false
+let myvar5 = undefined
+
+let intro = {
+    name: 'paras',
+    age: 999,
+    "live in": 'new delhi',
+    function: () => "function in object",
+    greeting: 'good night'
+}
+
+console.log(intro['live in']);
+console.log(intro.name);
+console.log(intro.function());
+
+let array = ['paras', 99, { function: function run() { return ('function in array ') } }, 34, 33,9, 54]
+console.log(array[2].function());
+console.log(array['0']);
+console.log(array.sort());
+console.log(array.push('add a new arrary in the end '));
+console.log(array);
+console.log(array.pop());
+console.log(array);
+console.log(array.shift());
+console.log(array);
+
+console.log(array.unshift('add a new array in the start'));
+console.log(array);
 
 
 
+let arrray = new Array(10)
+console.log(arrray[0]);
 
+arrray[0] = []
+// arrray[0] = [33,true]
+arrray[0].push('pushed')
+
+console.log(arrray);
+arrray.push('pushed')
+console.log(arrray);
+
+
+
+// document.write(array[5].function())
+
+// document.write(intro.function())
 
