1af0c245068f76e3db37c13a2d3972b08d92bc75
html <newparas11422@gmail.com>
2022-05-07 04:39:30 +0530

    random N0

--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-let obj1 ={
-    names : ['paras', 'anuj', 'rohan','sohan', 'mohan'],
-    greeting : 'HI! ',
-    speak(){
-      this.names.forEach((e)=>{
-       console.log(this.greeting + 'how are you? ' + e);
-      })
-    }
-}
 
-obj1.speak() 
 
-let obj2 ={
-    names : ['2paras2', '2anuj2', '2rohan2','2sohan2', '2mohan2'],
-    greeting : 'GOOD BYE! ',
-    speak(){
-      this.names.forEach(function(e){
-       console.log(this.greeting + 'how are you? ' + e);
-      })
-    }
-}
-obj2.speak() 
+ 
+  // console.log(rN1_100);
 
-console.log("This is date and time tutorial");
-let now = new Date();
-console.log(now);
-
-let dt = new Date(1000);
-console.log(dt);
-
-// let newDate = new Date("2029-09-30");
-// console.log(newDate)
-
-// let newDate = new Date(year, month, date, hours, minutes, seconds, milliseconds);
-let newDate = new Date(3020, 4, 6, 9, 3, 2, 34);
-console.log(newDate);
-
-let yr = newDate.getFullYear();
-console.log("The year is ", yr);
-
-let dat = newDate.getDate();
-console.log("The date is ", dat);
-
-let month = newDate.getMonth();
-console.log("The month is ", month);
-
-let hours = newDate.getHours();
-console.log("The hours is ", hours);
-
-newDate.setDate(8);
-newDate.setMinutes(29);
-console.log(newDate)
-setInterval(updateTime, 1000);
-
-function updateTime() {
-    time.innerHTML = new Date();
-}
-
+  const rNo = (a, b)=>{
+    //  a = 1
+    //  b= 100
+    let rN1_100 = Math.floor(a + (b - a)*Math.random())
+   document.getElementById('time').innerHTML = rN1_100
+  }
